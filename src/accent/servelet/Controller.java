0687c682dd9578d2ffa07bc470076e75ed86ef0f
at-dungvan <dung.van@asiantech.vn>
2017-11-27 11:57:41 +0700

    first commit

---
+++ src/accent/servelet/Controller.java
@@ -0,0 +1,60 @@
+package accent.servelet;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import accent.prediction.AccentPredictor;
+import net.sf.json.JSONObject;
+
+/**
+ * Servlet implementation class controller
+ */
+@WebServlet("/")
+public class Controller extends HttpServlet {
+	static AccentPredictor ap = new AccentPredictor();
+	
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Controller() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append(new File("").getAbsolutePath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF8");
+		response.setContentType("application/json");
+		StringBuilder buffer = new StringBuilder();
+	    BufferedReader reader = request.getReader();
+	    String line;
+	    while ((line = reader.readLine()) != null) {
+	        buffer.append(line);
+	    }
+	    String data = buffer.toString();
+	    String resData = ap.predictAccents(data);
+	    resData = "\"" + resData + "\"";
+		response.getWriter().write(resData);
+	}
+
+}
---
+++ src/utils/AccuracyCalculator.java
@@ -0,0 +1,58 @@
+package utils;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import accent.prediction.AccentPredictor;
+
+public class AccuracyCalculator {
+	AccentPredictor ld ;
+	public AccuracyCalculator() {
+		ld = new AccentPredictor();
+	}
+	
+	public double getAccuracy(String fileIn) {
+		FileProcessor fp = new FileProcessor();
+		String input = fp.readFileNew(fileIn);
+		input = Utils.normaliseString(input);
+		String[] inputSentence = input.split("[\\.\\!\\,\n\\;\\?]");
+		String clearSign = CompareString.getUnsignedString(input).trim();
+		Date start = new Date();
+		String out = ld.predictAccents(clearSign.trim());
+		double processedTime = (new Date().getTime() - start.getTime())*1.0/1000;
+		System.out.println("Processed time: " + processedTime + " seconds");
+		String output[] = out.split("\n");
+		System.out.println("Speed: " + output.length*1.0/processedTime + " sents/second");
+		System.out.println("Speed: " + out.split("\\s+").length*1.0/processedTime + " words/second");
+		int countAll = 1;;
+		int countMatch = 0;
+		
+		for (int i = 0; i < inputSentence.length; i++) {
+			//System.out.println(inputSentence[i] +"\n" + output[i]);
+			String wordsIn[] = Utils.normaliseString(inputSentence[i]).trim().split(" ");
+			String wordsOut[] = output[i].trim().split(" ");
+			boolean shouldPrint = false;
+			if (wordsIn.length == wordsOut.length) 
+				for (int j = 0; j < wordsOut.length; j++) {
+					if (wordsIn[j].trim().equalsIgnoreCase(wordsOut[j].trim())) countMatch ++;
+					else {
+						shouldPrint = true;
+					}
+					countAll++;
+				}
+			if(shouldPrint) {
+				System.out.println("input: " + inputSentence[i]);
+				System.out.println("output: " + output[i]);
+			}
+		}
+		System.out.println("Correct:"+countMatch);
+		System.out.println("All:"+countAll);
+		return new BigDecimal(((double)countMatch*100)/countAll).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+	}
+	
+	public static void main(String[] args) throws IOException {
+		AccuracyCalculator ac = new AccuracyCalculator();
+		System.out.println("Accuracy:" + ac.getAccuracy("datasets/test.txt") +"%");
+	}
+	
+}
